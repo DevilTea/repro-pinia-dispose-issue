849b38d5ec145d3e48ba0d777e178b525ebd55de
DevilTea <ch870814@gmail.com>
2022-11-23 21:36:17 +0800

    init

---
+++ src/components/TheCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Count: {{ store.count }}</h1>
+  <button @click="store.increment">Increment</button>
+</template>
+
+<script setup>
+import { onUnmounted } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+
+onUnmounted(() => {
+  store.$dispose()
+})
+</script>
